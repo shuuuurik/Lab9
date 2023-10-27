d73d443e923b65e4445782d614f2f7fb8c1b13e7
Mayorov Alexander <mayorov.shurik@mail.ru>
2023-10-28 00:10:30 +0300

    Initial commit

---
+++ Ex1/MeasuringDevice/ILoggingMeasuringDevice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeasuringDevice
+{
+    // TODO: Implement the ILoggingMeasuringDevice interface.
+    public interface ILoggingMeasuringDevice : IMeasuringDevice
+    {
+        /// <summary> 
+        /// Returns the file name of the logging file for the device. 
+        /// </summary> 
+        /// <returns>The file name for the logging file.</returns> 
+        string GetLoggingFile();
+    }
+}
